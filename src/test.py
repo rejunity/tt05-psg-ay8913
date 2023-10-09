222ac12aee1ff2ea056e314aad665ef3d8de9ffd
rej <rzioma@acm.org>
2023-10-09 11:12:11 +0200

    Updating tests for AY

--- src/test.py
+++ src/test.py
@@ -3,32 +3,50 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 def print_chip_state(dut):
-    try:
-        internal = dut.tt_um_rejunity_sn76489_uut
+    # try:
+        internal = dut.tt_um_rejunity_ay8913_uut
         print(
-            '{:4d}'.format(int(internal.tone[0].gen.compare.value)),
-            '{:4d}'.format(int(internal.tone[0].gen.counter.value)),
-                        "|#|" if internal.tone[0].gen.out == 1 else "|-|", # "|",
-            '{:4d}'.format(int(internal.tone[1].gen.compare.value)),
-            '{:4d}'.format(int(internal.tone[1].gen.counter.value)),
-                        "|#|" if internal.tone[1].gen.out == 1 else "|-|",  #"|",
-            '{:4d}'.format(int(internal.tone[2].gen.compare.value)),
-            '{:4d}'.format(int(internal.tone[2].gen.counter.value)),
-                        "|#|" if internal.tone[2].gen.out == 1 else "|-|",  #"!",
-            internal.noise[0].gen.control.value,
-            internal.noise[0].gen.reset_lfsr.value,
-            '{:4d}'.format(int(internal.noise[0].gen.tone.compare.value)),
-            '{:4d}'.format(int(internal.noise[0].gen.tone.counter.value)),
-                        ">" if internal.noise[0].gen.tone.out == 1 else " ",
-            internal.noise[0].gen.lfsr.value, ">>",
+            '{:4d}'.format(int(internal.tone_A_generator.period.value)),
+            '{:4d}'.format(int(internal.tone_A_generator.counter.value)),
+                        "|#|" if internal.tone_A_generator.out == 1 else "|-|", # "|",
+            '{:4d}'.format(int(internal.tone_B_generator.period.value)),
+            '{:4d}'.format(int(internal.tone_B_generator.counter.value)),
+                        "|#|" if internal.tone_B_generator.out == 1 else "|-|", # "|",
+            '{:4d}'.format(int(internal.tone_C_generator.period.value)),
+            '{:4d}'.format(int(internal.tone_C_generator.counter.value)),
+                        "|#|" if internal.tone_C_generator.out == 1 else "|-|",  #"!",
+            '{:4d}'.format(int(internal.noise_generator.tone.period.value)),
+            '{:4d}'.format(int(internal.noise_generator.tone.counter.value)),
+                        ">" if internal.noise_generator.tone.out == 1 else " ",
+            internal.noise_generator.lfsr.value, ">>",
             '{:3d}'.format(int(dut.uo_out.value >> 1)),
                         "@" if dut.uo_out[0].value == 1 else ".")
-    except:
-        print(dut.uo_out.value)
+    # except:
+        # print(dut.uo_out.value)
 
 @cocotb.test()
 async def test_psg(dut):
 
+    dut._log.info("start")
+    clock = Clock(dut.clk, 10, units="us")
+    cocotb.start_soon(clock.start())
+
+    # print_chip_state(dut)
+
+    dut._log.info("reset")
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
+
+    print_chip_state(dut)
+
+    dut._log.info("run")
+    for i in range(32):
+        await ClockCycles(dut.clk, 16)
+        print_chip_state(dut)
+
+
+async def test_sn(dut):
     dut._log.info("start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
