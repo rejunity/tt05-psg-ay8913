8acdc4b2b2401d0d6c35f6f7a5cbc08e49c2c3ee
rej <rzioma@acm.org>
2023-10-10 22:54:42 +0200

    Second flip-flop in signal_edge should prevent loosing 1st negedge if signal was 1 on resete

--- src/signal_edge.v
+++ src/signal_edge.v
@@ -6,15 +6,19 @@ module signal_edge(
     output wire on_negedge,
     output wire on_edge
 );
-    reg previous_signal_state;
+    reg previous_signal_state_0;
+    reg previous_signal_state_1;
     always @(posedge clk) begin
-        if (reset)
-            previous_signal_state <= 0;
-        else
-            previous_signal_state <= signal;
+        if (reset) begin
+            previous_signal_state_0 <= 0; // tested in posedge, init to 0 to prevent missing the first positive edge after reset
+            previous_signal_state_1 <= 1; // tested in negedge,               --- // ---               negative edge after reset
+        end else begin
+            previous_signal_state_0 <= signal;
+            previous_signal_state_1 <= signal;
+        end
     end
     
-    assign on_edge    = (previous_signal_state != signal);
-    assign on_posedge = (previous_signal_state != signal &&  signal);
-    assign on_negedge = (previous_signal_state != signal && !signal);
+    assign on_edge    = on_posedge | on_negedge;
+    assign on_posedge = (previous_signal_state_0 != signal &&  signal);
+    assign on_negedge = (previous_signal_state_1 != signal && !signal);
 endmodule
