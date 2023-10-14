6b16c6015bdf875077478c15132020d1f325b553
rej <rzioma@acm.org>
2023-10-14 14:26:51 +0200

    Simple hardcoded table works with Gate Level, lets try automatically calculated.

--- src/attenuation.v
+++ src/attenuation.v
@@ -108,40 +108,41 @@ module attenuation #( parameter CONTROL_BITS = 4, parameter VOLUME_BITS = 15 ) (
             // 14: out = 12'hCE4;
             // 15: out = 12'hFFF;
 
-             0: out = 8'h00;
-             1: out = 8'h03;
-             2: out = 8'h04;
-             3: out = 8'h06;
-             4: out = 8'h08;
-             5: out = 8'h0C;
-             6: out = 8'h11;
-             7: out = 8'h1C;
-             8: out = 8'h20;
-             9: out = 8'h35;
-            10: out = 8'h4B;
-            11: out = 8'h5F;
-            12: out = 8'h7E;
-            13: out = 8'hA3;
-            14: out = 8'hCE;
-            15: out = 8'hFF;
+            //  0: out = 8'h00;
+            //  1: out = 8'h03;
+            //  2: out = 8'h04;
+            //  3: out = 8'h06;
+            //  4: out = 8'h08;
+            //  5: out = 8'h0C;
+            //  6: out = 8'h11;
+            //  7: out = 8'h1C;
+            //  8: out = 8'h20;
+            //  9: out = 8'h35;
+            // 10: out = 8'h4B;
+            // 11: out = 8'h5F;
+            // 12: out = 8'h7E;
+            // 13: out = 8'hA3;
+            // 14: out = 8'hCE;
+            // 15: out = 8'hFF;
 
-            // // YM2149, numbers from the manual, every 2nd step is taken
-            // // YM2149 32 steps: 1V, .841, .707, .595, .5, .42, .354, .297, .25, .21, .177, .149, .125
-            // 15: out =           MAX_VOLUME;
-            // 14: out = `ATLEAST1(MAX_VOLUME * 0.707);
-            // 13: out = `ATLEAST1(MAX_VOLUME * 0.5  );
-            // 12: out = `ATLEAST1(MAX_VOLUME * 0.354);
-            // 11: out = `ATLEAST1(MAX_VOLUME * 0.25 );
-            // 10: out = `ATLEAST1(MAX_VOLUME * 0.177);   
-            // 9:  out = `ATLEAST1(MAX_VOLUME * 0.125);
-            // 8:  out = `ATLEAST1(MAX_VOLUME * 0.089);
-            // 7:  out = `ATLEAST1(MAX_VOLUME * 0.063);
-            // 6:  out = `ATLEAST1(MAX_VOLUME * 0.045);
-            // 5:  out = `ATLEAST1(MAX_VOLUME * 0.032);
-            // 4:  out = `ATLEAST1(MAX_VOLUME * 0.023);
-            // 3:  out = `ATLEAST1(MAX_VOLUME * 0.016);
-            // 2:  out = `ATLEAST1(MAX_VOLUME * 0.012);
-            // 1:  out = `ATLEAST1(MAX_VOLUME * 0.008);
+            // YM2149, numbers from the manual, every 2nd step is taken
+            // YM2149 32 steps: 1V, .841, .707, .595, .5, .42, .354, .297, .25, .21, .177, .149, .125
+            15: out =           MAX_VOLUME;
+            14: out = `ATLEAST1(MAX_VOLUME * 0.707);
+            13: out = `ATLEAST1(MAX_VOLUME * 0.5  );
+            12: out = `ATLEAST1(MAX_VOLUME * 0.354);
+            11: out = `ATLEAST1(MAX_VOLUME * 0.25 );
+            10: out = `ATLEAST1(MAX_VOLUME * 0.177);   
+            9:  out = `ATLEAST1(MAX_VOLUME * 0.125);
+            8:  out = `ATLEAST1(MAX_VOLUME * 0.089);
+            7:  out = `ATLEAST1(MAX_VOLUME * 0.063);
+            6:  out = `ATLEAST1(MAX_VOLUME * 0.045);
+            5:  out = `ATLEAST1(MAX_VOLUME * 0.032);
+            4:  out = `ATLEAST1(MAX_VOLUME * 0.023);
+            3:  out = `ATLEAST1(MAX_VOLUME * 0.016);
+            2:  out = `ATLEAST1(MAX_VOLUME * 0.012);
+            1:  out = `ATLEAST1(MAX_VOLUME * 0.008);
+            0:  out =           0;
 
             // Somewhat weird numbers from the original AY-3-8190 documentation
             // 15: out =           MAX_VOLUME;
