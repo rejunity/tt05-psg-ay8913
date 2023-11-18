010ae2f405b043b587e76abf07918eeaa74a925e
rej <rzioma@acm.org>
2023-11-18 08:47:25 +0100

    Script for batch recording

---
+++ src/record.sh
@@ -0,0 +1,17 @@
+make MODULE=record VGM=../music/Arcanoid_01Story.vgz
+make MODULE=record VGM=../music/Stormloard_03Start.atari_st.vgz
+make MODULE=record VGM=../music/PenguinAdventure_05IcePath.msx.vgz
+make MODULE=record VGM=../music/Turrican_01Welcome.atari_st.vgz
+
+make MODULE=record VGM=../music/TurricanII_04HiGIJoe.atari_st.vgz 			MAX_TIME=15
+make MODULE=record VGM=../music/Outrun_01MagicalSoundShower.zx.vgz 			MAX_TIME=15
+make MODULE=record VGM=../music/LEDStorm_01TitleTheme.zx.vgz 				MAX_TIME=15
+make MODULE=record VGM=../music/EnduroRacer_02MainBGM.atari_st.vgz 			MAX_TIME=15
+make MODULE=record VGM=../music/WoD_06TheLethalSwamp.atari_st.vgz 			MAX_TIME=15
+
+make MODULE=record VGM=../music/Stormlord_01MainTheme.atari_st.vgz 			MAX_TIME=10
+make MODULE=record VGM=../music/TurricanII_01TheFinalFight.atari_st.vgz 	MAX_TIME=10
+
+make MODULE=record VGM=../music/Turrican_02ShootOrDie.atari_st.vgz 			MAX_TIME=5
+make MODULE=record VGM=../music/TurricanII_12CheckTheGripper.atari_st.vgz 	MAX_TIME=5
+make MODULE=record VGM=../music/WoD_08ColdLandsofDoomandDespair.atari_st.vgz MAX_TIME=5
